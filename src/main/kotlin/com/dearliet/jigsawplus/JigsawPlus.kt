4dc4d652951c148426df7290cc44ad98c50352b9
Dearliet <lonesomecouchpotato@gmail.com>
2023-07-31 12:50:00 +0200

    1.20.1 Support

--- src/main/kotlin/com/dearliet/jigsawplus/JigsawPlus.kt
+++ src/main/kotlin/com/dearliet/jigsawplus/JigsawPlus.kt
@@ -5,24 +5,35 @@ import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
+import dev.jorel.commandapi.CommandAPICommand
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.entity.BlockDisplay
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.ItemDisplay
 import org.bukkit.plugin.java.JavaPlugin
 
 class JigsawPlus : JavaPlugin() {
 
     override fun onLoad() {
         super.onLoad()
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).silentLogs(true))
         JigsawCommand.register()
         registerPacketListener()
     }
 
     override fun onEnable() {
         super.onEnable()
+        CommandAPI.onEnable()
         saveDefaultConfig()
         globalJigsawLevel = config.getInt("globalJigsawLevel")
     }
 
     override fun onDisable() {
         super.onDisable()
+        CommandAPI.onDisable()
         config.set("globalJigsawLevel", globalJigsawLevel)
         saveConfig()
     }
